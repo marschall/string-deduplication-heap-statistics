9222e3fde8ccdeea61e295253755f407d6cf226c
Philippe Marschall <philippe.marschall@gmail.com>
2024-06-24 19:53:38 +0200

    cleanup

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-${JAVA_HOME}/bin/java -cp lib/eclipse-collections-api-11.1.0.jar:lib/eclipse-collections-11.1.0.jar:lib/string-deduplication-heap-statistics-1.0.0.jar \
+${JAVA_HOME}/bin/java -cp lib/eclipse-collections-api-11.1.0.jar:lib/eclipse-collections-11.1.0.jar:lib/org-graalvm-visualvm-lib-jfluid-heap-2.1.8.jar:lib/string-deduplication-heap-statistics-1.0.0.jar \
   -Xmx256m \
   -XX:+UseSerialGC \
   -Djava.awt.headless=true \
