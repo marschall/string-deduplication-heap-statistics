9eec7e463221728cab1ee6f8e9237a1b7496c833
Philippe Marschall <philippe.marschall@gmail.com>
2024-06-25 21:55:41 +0200

    fix script

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+set -u
 ${JAVA_HOME}/bin/java -cp lib/eclipse-collections-api-11.1.0.jar:lib/eclipse-collections-11.1.0.jar:lib/org-graalvm-visualvm-lib-jfluid-heap-2.1.8.jar:lib/string-deduplication-heap-statistics-1.0.0.jar \
   -Xmx256m \
   -XX:+UseSerialGC \
   -Djava.awt.headless=true \
   -XX:+UseStringDeduplication \
   -XX:-EnableDynamicAgentLoading \
-  com.github.marschall.stringdeduplicationheapstatistics.HeapParser
+  com.github.marschall.stringdeduplicationheapstatistics.HeapParser $@
